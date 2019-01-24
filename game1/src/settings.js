0cae1c52c9295cf6e17251764629c5c95f7e1d94
lujing <1042351095@qq.com>
2019-01-24 18:01:31 +0800

    1.0.7

---
+++ game1/src/settings.js
@@ -0,0 +1,55 @@
+window._CCSettings = {
+    platform: "ios",
+    groupList: [
+        "default"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ]
+    ],
+    rawAssets: {
+        assets: {
+            "b6TEnS+pJI47V8YzJWdIoe": [
+                "Texture/Dragon_tex.png",
+                "cc.Texture2D"
+            ],
+            a8Anh32NZGRZegUtSgEj26: [
+                "Texture/singleColor.png",
+                "cc.Texture2D"
+            ]
+        },
+        internal: {
+            "71VhFCTINJM6/Ky3oX9nBT": [
+                "image/default_btn_disabled.png",
+                "cc.Texture2D"
+            ],
+            "e8Ueib+qJEhL6mXAHdnwbi": [
+                "image/default_btn_normal.png",
+                "cc.Texture2D"
+            ],
+            "b4P/PCArtIdIH38t6mlw8Y": [
+                "image/default_btn_pressed.png",
+                "cc.Texture2D"
+            ]
+        }
+    },
+    launchScene: "db://assets/Scene/dragon.fire",
+    scenes: [
+        {
+            url: "db://assets/Scene/dragon.fire",
+            uuid: "422iOW2LRPU5/2GYGLJj2i"
+        }
+    ],
+    packedAssets: {
+        "03ad0803a": [
+            "29FYIk+N1GYaeWH/q1NxQO",
+            "41D7kWhyFGY7q4NDlzkazn",
+            "422iOW2LRPU5/2GYGLJj2i",
+            "e97GVMl6JHh5Ml5qEDdSGa",
+            "f0BIwQ8D5Ml7nTNQbh1YlS"
+        ]
+    },
+    orientation: "",
+    debug: true
+};
